f25fa602604294dff29c181c9e12d723afe7f915
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-19 04:33:42 -0500

    install typerwriter effect and framer motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.title = `Home`;
+  });
 
-    useEffect(() => {
-        document.title = `Home`
-    })
-
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>
+        either use Typewrite on home page or get rid of it in package.json
+        https://www.npmjs.com/package/typewriter-effect
+      </h1>
+    </>
+  );
 };
 
-export { Home }
+export { Home };
