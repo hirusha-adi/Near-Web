8d05ed06890f8e6aa2f872a9b82c759a0671cba0
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-20 09:18:23 -0500

    update btn classes

--- src/pages/Home/components/Introduction.jsx
+++ src/pages/Home/components/Introduction.jsx
@@ -46,17 +46,11 @@ const Introduction = () => {
                 doloremque laudantium, totam rem aperiam, eaque ipsa quae.
               </p>
               <div className="flex items-center gap-x-3 sm:text-sm">
-                <a
-                  href="javascript:void(0)"
-                  className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
-                >
+                <a href="javascript:void(0)" className="btn">
                   Get started
                   <ChevronRightIcon className="w-5" />
                 </a>
-                <a
-                  href="javascript:void(0)"
-                  className="flex items-center justify-center gap-x-1 py-2 px-4 text-gray-700 hover:text-gray-900 font-medium duration-150 md:inline-flex"
-                >
+                <a href="javascript:void(0)" className="btn btn-ghost">
                   Contact sales
                   <ChevronRightIcon className="w-5" />
                 </a>
