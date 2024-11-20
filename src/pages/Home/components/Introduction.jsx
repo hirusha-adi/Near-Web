1004676e727e8625818de67ca1f4b2898eeec1bc
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-20 09:39:49 -0500

    change layout and stuff

--- src/pages/Home/components/Introduction.jsx
+++ src/pages/Home/components/Introduction.jsx
@@ -1,5 +1,6 @@
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import ImagePandaFace from "../../../assets/face.png";
+import ImagePandaSitting from "../../../assets/sitting.png";
 
 const Introduction = () => {
   return (
@@ -13,33 +14,31 @@ const Introduction = () => {
       ></div>
       <div className="relative">
         <section>
-          <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-16 md:flex">
+          <div className="max-w-screen-xl mx-auto px-4 pb-28 pt-12 md:pt-24 gap-12 text-gray-600 overflow-hidden md:px-16 md:flex md:flex-row-reverse">
+            {/* Image Section */}
+            <div className="flex justify-center md:block md:flex-1 order-first md:order-none">
+              <img
+                src={ImagePandaSitting}
+                className="w-48 sm:w-64 md:w-96 mx-auto block"
+                alt="Panda Face"
+              />
+            </div>
+            {/* Text Section */}
             <div className="flex-none space-y-5 max-w-xl">
               <a
                 href="javascript:void(0)"
-                className="inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white"
+                className="gap-x-6 items-center rounded-2xl p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white hidden md:inline-flex glass"
               >
-                <span className="inline-block rounded-full px-3 py-1 bg-red-600 text-white">
+                <span className="inline-block rounded-2xl px-3 py-1 bg-red-600 text-white">
                   News
                 </span>
                 <p className="flex items-center">
                   Version 8.0 has been released!
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 h-5"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <ChevronRightIcon className="w-4" />
                 </p>
               </a>
-              <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
-                Build your SaaS exactly how you want
+              <h1 className="text-4xl text-gray-900 font-extrabold sm:text-5xl">
+                I am Near, The discord of TeamSDS.
               </h1>
               <p>
                 Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
@@ -55,10 +54,6 @@ const Introduction = () => {
                   <ChevronRightIcon className="w-5" />
                 </a>
               </div>
-            </div>
-            <div className="flex-1 md:block">
-              {/* Replace with your image */}
-              <img src={ImagePandaFace} className="w-96" />
             </div>
           </div>
         </section>
