65ca07b6a66ba99f4c07060d57d803b6a366077c
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-21 11:08:21 -0500

    basic comments

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -13,20 +13,18 @@ const AdminLogin = () => {
         <div className="grid md:grid-cols-2 items-center gap-4 max-w-6xl w-full">
           <div className="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
             <form className="space-y-4">
+              {/* Topic */}
               <div className="mb-8">
-                <h3 className="text-gray-800 text-3xl font-extrabold">
-                  Sign in
-                </h3>
-                <p className="text-gray-500 text-sm mt-4 leading-relaxed">
+                <h3 className="text-3xl font-extrabold">Sign in</h3>
+                <p className="text-sm mt-4 leading-relaxed">
                   Sign in to manage Near and all related settings.
                 </p>
               </div>
-
+              {/* Fields */}
               <label className="input input-bordered flex items-center gap-2">
                 <UserIcon className="w-5" />
                 <input type="text" className="grow" placeholder="Username" />
               </label>
-
               <label className="input input-bordered flex items-center gap-2">
                 <KeyIcon className="w-5" />
                 <input
@@ -35,7 +33,7 @@ const AdminLogin = () => {
                   placeholder="Password"
                 />
               </label>
-
+              {/* Login Button */}
               <div className="!mt-8">
                 <button
                   type="button"
@@ -44,7 +42,7 @@ const AdminLogin = () => {
                   Login
                 </button>
               </div>
-
+              {/* Contact */}
               <p className="text-sm !mt-8 text-center text-gray-800">
                 Don&apos;t have an account?{" "}
                 <a
@@ -56,6 +54,7 @@ const AdminLogin = () => {
               </p>
             </form>
           </div>
+          {/* Image */}
           <div className="lg:h-[400px] md:h-[300px] max-md:mt-8">
             <img
               src="https://readymadeui.com/login-image.webp"
