d438f9c86825e6cc9054c2a7352f8f5ddeb4a77c
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-21 12:26:18 -0500

    auth access works

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,5 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import { Home, Leaderboards, Help, AdminLogin, PageNotFound } from "../pages";
+import {
+  Home,
+  Leaderboards,
+  Help,
+  AdminLogin,
+  AdminHome,
+  PageNotFound,
+} from "../pages";
+import { isUserLoggedIn } from "../lib/backend";
+import { Navigate } from "react-router-dom";
 
 const AllRoutes = () => {
   return (
@@ -9,10 +18,23 @@ const AllRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/leaderboards" element={<Leaderboards />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
+
+        {/* Admin Pages */}
+        <Route path="/admin" element={<Navigate to="/admin/login" />} />
+        <Route
+          path="/admin/login"
+          element={
+            isUserLoggedIn ? <Navigate to="/admin/home" /> : <AdminLogin />
+          }
+        />
+        <Route
+          path="/admin/home"
+          element={isUserLoggedIn ? <AdminHome /> : <Navigate to="/404" />}
+        />
 
         {/* Errors */}
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<PageNotFound />} />
       </Routes>
     </>
   );
