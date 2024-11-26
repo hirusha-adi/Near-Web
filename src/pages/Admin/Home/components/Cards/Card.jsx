2de7c83d7996c31cda366c9fab3052ca798c3467
hirusha-adi <hirushaadi@gmail.com>
2024-11-26 14:59:00 +0530

    reformat to 2 space tab

--- src/pages/Admin/Home/components/Cards/Card.jsx
+++ src/pages/Admin/Home/components/Cards/Card.jsx
@@ -1,20 +1,20 @@
 const Card = ({ icon, number, description }) => {
-    return (
-        <>
-            <div className="flex items-center p-4 bg-base-100 shadow-xl rounded-2xl">
-                {/* Icon */}
-                <div className="text-red-600 text-4xl mr-4 w-10">
-                    {icon}
-                </div>
+	return (
+		<>
+			<div className="flex items-center p-4 bg-base-100 shadow-xl rounded-2xl">
+				{/* Icon */}
+				<div className="text-red-600 text-4xl mr-4 w-10">
+					{icon}
+				</div>
 
-                {/* Content */}
-                <div className="flex flex-col">
-                    <div className="text-3xl font-bold">{number}</div>
-                    <div className="text-sm text-gray-500">{description}</div>
-                </div>
-            </div>
-        </>
-    );
+				{/* Content */}
+				<div className="flex flex-col">
+					<div className="text-3xl font-bold">{number}</div>
+					<div className="text-sm text-gray-500">{description}</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export { Card };
