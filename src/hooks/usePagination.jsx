3d7876ec04318b2d40faf207600b9f9509473c5d
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-29 22:53:10 +0530

    dont exceed the max page, else, it will return empty list

--- src/hooks/usePagination.jsx
+++ src/hooks/usePagination.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export function usePagination(initialPage = 1, minPage = 1) {
+export function usePagination(initialPage = 1, minPage = 1, initialMaxPage = 1) {
   const [currentPage, setCurrentPage] = useState(initialPage);
+  const [maxPage, setMaxPage] = useState(initialMaxPage);
 
-  const nextPage = () => setCurrentPage((prev) => prev + 1);
+  const nextPage = () =>
+    setCurrentPage((prev) => (prev < maxPage ? prev + 1 : maxPage));
 
   const previousPage = () =>
     setCurrentPage((prev) => Math.max(prev - 1, minPage));
 
   const resetPage = () => setCurrentPage(initialPage);
 
-  return { currentPage, nextPage, previousPage, resetPage, setPage: setCurrentPage };
+  // Ensure currentPage does not exceed maxPage when maxPage changes
+  useEffect(() => {
+    if (currentPage > maxPage) {
+      setCurrentPage(maxPage);
+    }
+  }, [maxPage, currentPage]);
+
+  return {
+    currentPage,
+    nextPage,
+    previousPage,
+    resetPage,
+    setPage: setCurrentPage,
+    maxPage,
+    setMaxPage,
+  };
 }
