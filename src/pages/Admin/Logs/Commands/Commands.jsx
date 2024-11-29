c92526eb32e062ae063169308d95a07b7d4e626d
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-29 22:42:23 +0530

    it works

--- src/pages/Admin/Logs/Commands/Commands.jsx
+++ src/pages/Admin/Logs/Commands/Commands.jsx
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
 import { DrawerAndLayout } from "../../components/DrawerAndLayout";
+import { getCommandsHistory } from "../../../../lib/backend";
+import { useFetchPocketbase, usePagination } from "../../../../hooks";
 
 const Commands = () => {
 	useEffect(() => {
 		document.title = `Commands History | Near`;
 	});
 
+	const { currentPage, nextPage, previousPage, resetPage, setPage } = usePagination(1);
+	const { data: commands, loading, error } = useFetchPocketbase(getCommandsHistory, currentPage);
+
+	
+	console.log(commands?.items)
+
 	return (
 		<>
 			<DrawerAndLayout pageTitle={"Command Logs (History)"} locationBreadcrumbs={["Settings", "Logs", "Commands"]}>
-				Page
+				<div onClick={() => nextPage(1)}>
+					Click here
+				</div>
 			</DrawerAndLayout>
 		</>
 	);
